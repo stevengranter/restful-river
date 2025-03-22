3487e365f9e707e6468d1e36b909ab66e4b59554
Steven Granter <stevengranter@gmail.com>
2025-03-21 22:13:47 -0230

    Add root route /, index.html view Add catch-all route *, 404.html view Revise config for tsconfig.json Add .prettierrc, .prettierignore

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,41 @@
-console.log("Hello... you!")
+// src/server.ts
+
+// * Imports * //
+
+// External imports
+import 'dotenv/config'
+import express from 'express'
+import path from 'path'
+
+// Internal imports
+import { router as rootRouter } from './routes/root.route.js'
+import { SCRIPT_DIR, VIEWS_DIR } from './constants.js'
+import { PORT, HOST, PROTOCOL } from './constants.js'
+
+// * App setup * //
+const app = express()
+
+// * Middleware * //
+
+// Public static files
+app.use('/', express.static(path.join(SCRIPT_DIR, '../public')))
+
+// Home/Root route
+app.use('/', rootRouter)
+
+// 404 route
+app.use('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(VIEWS_DIR, '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 Not Founf' })
+    } else {
+        res.type('txt').send('404 Not Found')
+    }
+})
+
+// * Server setup * //
+app.listen(PORT, () =>
+    console.log(`Server running on ${PROTOCOL}://${HOST}:${PORT}`)
+)
